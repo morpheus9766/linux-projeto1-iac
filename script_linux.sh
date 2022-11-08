a28045d4b30b03a78e8bff9c6febefda41df0110
Luiz <morpheus9766@hotmail.com>
2022-11-08 09:06:26 -0300

    Script criar usuários e permissões

---
+++ script_linux.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "Criando diretorios..."
+mkdir /public
+mkdir /adm
+mkdir /ven
+mkdir /sec
+echo "Criando grupos de usuários..."
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM
+passwd carlos -e
+useradd maria -c "Maria" -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM
+passwd maria -e
+useradd joao -c "João" -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_ADM
+passwd joao -e
+
+useradd debora -c "Debora" -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN
+passwd debora -e
+useradd sebastiana -c "Sebastiana" -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN
+passwd sebastiana -e
+useradd roberto -c "Roberto" -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_VEN
+passwd roberto -e
+
+useradd josefina -c "Josefina" -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC
+passwd josefina -e
+useradd amanda -c "Amanda" -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC
+passwd amanda -e
+useradd rogerio -c "Rogerio" -s /bin/bash -m -p $(openssl passwd -crypt senha123) -G GRP_SEC
+passwd rogerio -e
+
+echo "Permissões..."
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /public
+
+echo "Finalizado!!!"
